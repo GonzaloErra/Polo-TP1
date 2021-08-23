7c8249633b5c8ff3fb13e6a6a0ae051f614a3f52
GonzaloErra <gonzaloerra@hotmail.com>
2021-08-23 18:06:46 -0300

    Agrega Proyecto

---
+++ src/Logica/Main.java
@@ -0,0 +1,24 @@
+package Logica;
+
+
+import IGU.Principal;
+//import java.security.Principal;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lorenzo
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//      Para que figure en pantalla la interfaz grafica
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
+    }
+    
+}
